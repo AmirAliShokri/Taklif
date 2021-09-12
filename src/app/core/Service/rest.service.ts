416082553a1d33cbdc8f2bae908c28a5f8d4ef89
AmirAliShokri <amirali.shokri12@gmail.com>
2021-09-12 20:12:50 +0430

    Config Rest Service

---
+++ src/app/core/Service/rest.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from './../../../environments/environment.prod';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RestService {
+  constructor(private client: HttpClient) {}
+
+  public getUrl(resourceName: string) {
+    return `${environment.baseUrl}${resourceName}`;
+  }
+
+  public getAll<TResonse>(resourceName: string): Observable<TResonse> {
+    return this.client.get<TResonse>(this.getUrl(resourceName));
+  }
+}
